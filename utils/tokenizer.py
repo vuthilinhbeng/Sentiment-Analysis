b3a6e89d94c7f9590010305303b6d6e898e663bb
vuthilinhbeng <106636882+vuthilinhbeng@users.noreply.github.com>
2024-05-09 13:09:22 +0700

    fix: delete libray don't use

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -4,8 +4,6 @@ import pandas as pd
 import numpy as np
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-import tensorflow as tf
-
 PRETRAINED_MODEL = 'vinai/phobert-base'
 
 def read_csv(file_path):
