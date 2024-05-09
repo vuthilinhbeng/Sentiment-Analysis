e898b208dc94dbf0c8cd5c6e03029de712e4f976
vuthilinhbeng <106636882+vuthilinhbeng@users.noreply.github.com>
2024-05-09 12:57:51 +0700

    fix: delete libray don't use

--- create_model.py
+++ create_model.py
@@ -2,10 +2,9 @@ from keras.models import Model
 from keras.layers import Input, Dense, Dropout, concatenate
 from keras.layers import Layer
 from tensorflow.keras.optimizers import AdamW
-from transformers import TFAutoModel
-from utils.variables import STEPS_PER_EPOCH, VALIDATION_STEPS, df_train,val_tf_dataset,train_tf_dataset
-from utils.config import MAX_SEQUENCE_LENGTH, BATCH_SIZE, EPOCHS, WEIGHT_DECAY, LEARNING_RATE, DROP_OUT
-from utils.tokenizer import PRETRAINED_MODEL
+from utils.variables import df_train
+from utils.config import MAX_SEQUENCE_LENGTH, WEIGHT_DECAY, LEARNING_RATE, DROP_OUT
+
 
 
 class CustomLayer(Layer):
